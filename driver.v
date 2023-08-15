40469149added53303a2e32aafb862df13e52f66
Thiago Santos <47781320+thiagoolvr@users.noreply.github.com>
2023-08-15 19:06:40 -0300

    Lab3: driver

---
+++ driver.v
@@ -0,0 +1,9 @@
+module driver #(
+    parameter WIDTH = 8)(
+    input wire data_en,
+	input wire [WIDTH-1 : 0] data_in,
+	output wire [WIDTH-1 : 0] data_out);
+
+	//assign data_out = data_en ? data_in : 'bz;
+    assign data_out = data_en ? data_in : {WIDTH{1'bz}};
+endmodule
